e17f74930806acb8e14f27460ea857a968467c91
Mariia Prokopenko <Klymenkomasha@gmail.com>
2022-10-12 19:29:25 +0300

    Добавляет решение первой задачи дз

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryPicture = createGaleryItem(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryPicture);
+galleryContainer.addEventListener('click', onPictureClick)
+
+function createGaleryItem(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </div>
+        `;
+    }).join('');
+}
+
+function onPictureClick(evt) {
+    evt.preventDefault()
+
+    const isGalleryImageEl = evt.target.classList.contains('gallery__image');
+
+    if (!isGalleryImageEl) {
+        return;
+    }
+
+    const modalWindow = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">
+    `)
+
+    modalWindow.show()
+
+    if (modalWindow.show()) {
+        document.addEventListener("keydown", onKeybordClick)
+
+        function onKeybordClick(evt) {
+            if (evt.key === 'Escape') {
+                modalWindow.close();
+            }     
+        }
+    }
+}
+
+
